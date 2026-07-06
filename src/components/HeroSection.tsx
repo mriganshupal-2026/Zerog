c2fce6a3b29f1549b87947d59f0344949c5db252
mriganshupal-2026 <mriganshupal@gmail.com>
2026-07-06 14:52:49 +0530

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -41,7 +41,7 @@ export function HeroSection() {
             variants={fadeUp}
             className="max-w-4xl text-5xl font-semibold leading-[1.02] tracking-normal sm:text-6xl lg:text-7xl"
           >
-            The World's Most Capable and Unique Vacuum Cleaner!
+            The World&apos;s Most Capable and Unique Vacuum Cleaner!
           </motion.h1>
           <motion.p
             variants={fadeUp}
